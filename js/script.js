a09fa24333470fe28381625793c0411d256166fd
Andrii <nikulin.and90@gmail.com>
2022-08-19 16:29:56 +0300

    isPalindrome

--- js/script.js
+++ js/script.js
@@ -46,41 +46,41 @@
                                        
                                     //  УСЛОВИЯ!!!
 
-if (4 == 4) {
-    console.log('yes');
+// if (4 == 4) {
+//     console.log('yes');
 
-}   else {
-    console.log('no');
-}                              
-
-
-const num = 50;
-
-if (num < 49) {
-    console.log('NO');
-
-} else if ( num > 100) {
-    console.log('не сегодня');
-    } else if (num == 50) {
-        console.log('yes');
-    }
+// }   else {
+//     console.log('no');
+// }                              
 
 
-    console.log( NaN || 2 || undefined );
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('NO');
+
+// } else if ( num > 100) {
+//     console.log('не сегодня');
+//     } else if (num == 50) {
+//         console.log('yes');
+//     }
+
+
+//     console.log( NaN || 2 || undefined );
  
-    console.log( NaN && 2 && undefined );
+//     console.log( NaN && 2 && undefined );
      
-    console.log( 1 && 2 && 3 );
+//     console.log( 1 && 2 && 3 );
      
-    console.log( !1 && 2 || !3 );
+//     console.log( !1 && 2 || !3 );
      
-    console.log( 25 || null && !3 );
+//     console.log( 25 || null && !3 );
      
-    console.log( NaN || null || !3 || undefined || 5);
+//     console.log( NaN || null || !3 || undefined || 5);
      
-    console.log( NaN || null && !3 && undefined || 5);
+//     console.log( NaN || null && !3 && undefined || 5);
      
-    console.log( 5 === 5 && 3 > 1 || 5);
+//     console.log( 5 === 5 && 3 > 1 || 5);
 
 
     // const hamburger = 3;
@@ -102,14 +102,65 @@ if (num < 49) {
 //    console.log('Done!');
 // }
 
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
  
-if (hamburger && cola || fries === 3 && nuggets) {
-   console.log('Done!');
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!');
+// }
+
+
+
+
+
+
+// const str = 'Hello, where are you from?';
+
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.indexOf('w'));
+// console.log(str.slice(0, 3));
+// console.log(str.substring(0, 3));
+// console.log(str.replace('Hello','Привет'));
+// console.log(str.trim());
+// console.log(str.split(''));
+// console.log(str.reverse());
+
+
+
+
+
+// const arr = ['Andri', 'Arthur', 'John'];
+
+// console.log(arr.length);
+// console.log(arr.push('Alex'));
+// console.log(arr);
+// console.log(arr.unshift('Sergey'));
+// console.log(arr);
+// console.log(arr.pop());
+// console.log(arr.shift());
+// console.log(arr.concat(['Ben']));
+// console.log(arr.toString());
+// console.log(arr.join(';'));
+
+isPalindrome('шала');
+
+function isPalindrome (str) {
+    let arr = str.split('');
+    let reversArr = arr.reverse();
+    let resString = reversArr.join('');
+    let result = str === resString;
+    console.log(result);
 }
-const sjhgkjh = {
-    
-}
+
+
+
+
+
+
+
+
+
+
